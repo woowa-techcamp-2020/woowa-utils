38c5af276d08f00ac901af979a8348d11a0b5434
andy nam <naamoonoo.y@gmail.com>
2020-07-31 14:06:27 +0900

    .

---
+++ src/utils/promiseHandler.ts
@@ -0,0 +1,7 @@
+const promiseHandler = async <T>(promise: Promise<T>) => {
+	return promise
+		.then((data: T) => [data, null])
+		.catch((error) => [null, error]);
+};
+
+export default promiseHandler;
